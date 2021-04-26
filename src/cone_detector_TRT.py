27065f01dfa908276b36a8f9ec892d6b1cfd6d69
Dominik <dominik.florjan@gmail.com>
2021-04-26 22:53:13 +0200

    Clean up, new predict function returning only bbox

--- src/cone_detector_TRT.py
+++ src/cone_detector_TRT.py
@@ -18,83 +18,54 @@ WINDOW_NAME = "Test"
 
 class ConeDetectorTRT:
     def __init__(self):
-        parser = argparse.ArgumentParser()
-        self.model = "yolov4-tiny-608"
+        self.conf_th = 0.3
+
+        self.model = "yolov4-tiny-416"
         if not os.path.isfile('yolo/%s.trt' % self.model):
             raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % self.model)
 
         h, w = get_input_shape(self.model)
-        cls_dict = ["Cone"]
-        vis = BBoxVisualization(cls_dict)
-        trt_yolo = TrtYOLO(self.model, (h, w), 1, False)
+        self.trt_yolo = TrtYOLO(self.model, (h, w), 1, False)
 
         # self.cam = add_camera_args(/dev/video1)
+        # Below 
+        parser = argparse.ArgumentParser()
         parser = add_camera_args(parser)
         args = parser.parse_args()
         cam = Camera(args)
         if not cam.isOpened():
             raise SystemExit('ERROR: failed to open camera!')
 
-        #  open_window(
-            #  WINDOW_NAME, 'Camera TensorRT YOLO Demo',
-            #  cam.img_width, cam.img_height)
-        #  self.loop_and_detect(cam, trt_yolo, conf_th=0.3, vis=vis)
-        self.predict(cam, trt_yolo, conf_th=0.3)
+        self.predict_debug(cam)
         cam.release()
         cv2.destroyAllWindows()
-        
-    def loop_and_detect(self, cam, trt_yolo, conf_th, vis):
-        """Continuously capture images from camera and do object detection.
 
-        # Arguments
-          cam: the camera instance (video source).
-          trt_yolo: the TRT YOLO object detector instance.
-          conf_th: confidence/score threshold for object detection.
-          vis: for visualization.
-        """
-        full_scrn = False
-        fps = 0.0
-        tic = time.time()
-        while True:
-            if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
-                break
-            img = cam.read()
-            if img is None:
-                break
-            boxes, confs, clss = trt_yolo.detect(img, conf_th)
-            img = vis.draw_bboxes(img, boxes, confs, clss)
-            img = show_fps(img, fps)
-            cv2.imshow(WINDOW_NAME, img)
-            toc = time.time()
-            curr_fps = 1.0 / (toc - tic)
-            # calculate an exponentially decaying average of fps number
-            fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
-            tic = toc
-            key = cv2.waitKey(1)
-            if key == 27:  # ESC key: quit program
-                break
-            elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
-                full_scrn = not full_scrn
-                set_display(WINDOW_NAME, full_scrn)
-
-    def predict(self, cam, trt_yolo, conf_th: float):
+    def predict_debug(self, cam):
         """ This needs to be changed to something that is returning
             the boxes array so it can be used in the pipeline
         """
         fps = 0.0
         tic = time.time()
         while True:
+            # cam.read() gets frame from camera, basically a cv2.VideoCapture with
+            # abstraction
             img = cam.read()
             if img is None:
                 break
-            boxes, confs, clss = trt_yolo.detect(img, conf_th)
+            #  boxes, confs, clss = self.trt_yolo.detect(img, conf_th)
+            boxes = self.predict(img)
+            print(boxes)
             toc = time.time()
             curr_fps = 1.0 / (toc - tic)
             # calculate an exponentially decaying average of fps number
             fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
             tic = toc
             print(fps)
-            #  print(boxes)
+
+    def predict(self, img):
+        boxes, _, _ = self.trt_yolo.detect(img, self.conf_th)
+        return np.array(boxes)
+
             
 if __name__ == '__main__':
     detect = ConeDetectorTRT()
